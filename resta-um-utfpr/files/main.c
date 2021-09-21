200c4f8bcfbe4ae1bf8decb019c00e1bccc3c2da
Willian Ayres <70988977+willianayres@users.noreply.github.com>
2021-09-20 23:03:31 -0300

    Add files via upload

---
+++ resta-um-utfpr/files/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<locale.h>
+#include<conio.h>
+#include<ctype.h>
+#include <time.h>
+#include "Menu.h"
+#include "Tabuleiro.h"
+#include "Principal.h"
+#include "Estatistica.h"
+
+
+
+int main(){
+    setlocale(LC_ALL,"portuguese");
+    system("title RestaUm");
+    Inicio();
+    return 0;
+}
+
+
+
